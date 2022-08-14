2f15e81020a96feb52a9759f796543eac0a11b99
GitteadorZeppelin <jorgeallozags@gmail.com>
2022-08-14 16:47:05 +0200

    Routing

--- src/routing/app-routing.module.ts
+++ src/routing/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokedexComponent } from 'src/app/pokedex/pokedex.component';
+import { PokemonComponent } from 'src/app/pokemon/pokemon.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PokedexComponent
+  },{
+    path: '/pokedex',
+    component: PokedexComponent
+  },{
+    path: '/pokemon/:num',
+    component: PokemonComponent
+  },{
+    path: '**',
+    component: PokedexComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
