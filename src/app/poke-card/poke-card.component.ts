fa3c513ee3741c241d99290c6799756540fc32b4
GitteadorZeppelin <jorgeallozags@gmail.com>
2022-08-14 17:14:10 +0200

    Cards

--- src/app/poke-card/poke-card.component.ts
+++ src/app/poke-card/poke-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-poke-card',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokeCardComponent implements OnInit {
 
+  //Variables
+  @Output() pkmn: any;
+  
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  creoClase(tipo1 :any, tipo2:any){
+    if(tipo2 == undefined){
+      return tipo1+"-top " + tipo1+"-left " + tipo1+"-right " + tipo1+"-bottom"
+    } else {
+      return tipo1+"-top " + tipo1+"-left " + tipo2.type.name+"-right " + tipo2.type.name+"-bottom"
+    }
+  }
 }
