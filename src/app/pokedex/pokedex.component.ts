b65ae69eb852b78bb6067cb980ba6daf95e38efa
GitteadorZeppelin <jorgeallozags@gmail.com>
2022-08-14 17:59:28 +0200

    Pokedex fixed

--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -10,34 +10,42 @@ export class PokedexComponent implements OnInit {
   //Variables
   listaPkmn: any;
   page: any;
-
+  numPkmnPag: any;
+  //Declaración de servicios en el constructor
   constructor(
     private apiKachu: APikachuService
   ) {
+    //Inicialización de varianles
     this.listaPkmn = []
-  }
-
-  ngOnInit(): void {
     this.page = 1;
+    this.numPkmnPag = 9;
+  }
+  //OnInit de obtener Pokemon 
+  ngOnInit(): void {
     this.getPkmn(this.page);
   }
-
+  //Obtenemos pokemon según la página
   async getPkmn(page: any) {
-    this.listaPkmn = [];
+    //Inicalizamos el contador a 1
     let contador = 1;
-    console.log(page)
-    for (let x = ((page-1)*12)+1; x <= (page*12); x++) {
+    //For con algoritmo
+    for (let x = ((page-1)*this.numPkmnPag)+1; x <= page*this.numPkmnPag; x++) {
+      //Llamada a la api asincorna
       await this.apiKachu.getPKMN(x).then(datos => {
-        this.listaPkmn[contador-1] = datos
-        contador++
+        this.listaPkmn[contador-1] = datos;
+        //Incremento
+        contador++;
+      }, err=>{ //Callback de error
+        console.log(err)
       }) 
     }    
   }
-
+  //Metodo que avanza las páginas
   adelante(){
     this.page++;
     this.getPkmn(this.page);
   }
+  //Método que retrocede las páginas
   atras(){
     this.page--;
     this.getPkmn(this.page);
