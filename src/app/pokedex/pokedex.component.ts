23eeae79020c8cf1602fe6fc7d4a77b39a1ffb8e
GitteadorZeppelin <jorgeallozags@gmail.com>
2022-08-14 17:15:29 +0200

    Pokedex component

--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { APikachuService } from '../servicios/a-pikachu.service';
 
 @Component({
   selector: 'app-pokedex',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokedex.component.css']
 })
 export class PokedexComponent implements OnInit {
+  //Variables
+  listaPkmn: any;
+  page: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private apiKachu: APikachuService
+  ) {
+    this.listaPkmn = []
   }
 
+  ngOnInit(): void {
+    this.page = 1;
+    this.getPkmn(this.page);
+  }
+
+  async getPkmn(page: any) {
+    this.listaPkmn = [];
+    let contador = 1;
+    console.log(page)
+    for (let x = ((page-1)*12)+1; x <= (page*12); x++) {
+      await this.apiKachu.getPKMN(x).then(datos => {
+        this.listaPkmn[contador-1] = datos
+        contador++
+      }) 
+    }    
+  }
+
+  adelante(){
+    this.page++;
+    this.getPkmn(this.page);
+  }
+  atras(){
+    this.page--;
+    this.getPkmn(this.page);
+  }
 }
