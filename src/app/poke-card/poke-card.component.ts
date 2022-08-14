0cbeba1901ed904a9fbba971eff1344efb1a3d9a
GitteadorZeppelin <jorgeallozags@gmail.com>
2022-08-14 17:34:18 +0200

    Poke-cards works

--- src/app/poke-card/poke-card.component.ts
+++ src/app/poke-card/poke-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-poke-card',
@@ -8,14 +8,14 @@ import { Component, OnInit, Output } from '@angular/core';
 export class PokeCardComponent implements OnInit {
 
   //Variables
-  @Output() pkmn: any;
-  
+  @Input() pkmn: any;
   constructor() { }
 
   ngOnInit(): void {
   }
 
   creoClase(tipo1 :any, tipo2:any){
+    console.log(this.pkmn.id)
     if(tipo2 == undefined){
       return tipo1+"-top " + tipo1+"-left " + tipo1+"-right " + tipo1+"-bottom"
     } else {
