1a104575596c61387dede7144d4b41398c4b8315
GitteadorZeppelin <jorgeallozags@gmail.com>
2022-08-14 16:44:01 +0200

    ng g c poke-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokedexComponent } from './pokedex/pokedex.component';
+import { PokeCardComponent } from './poke-card/poke-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PokedexComponent
+    PokedexComponent,
+    PokeCardComponent
   ],
   imports: [
     BrowserModule,
