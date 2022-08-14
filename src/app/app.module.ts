fe166c229f388f6bcb9623863d190f22641ddc9f
GitteadorZeppelin <jorgeallozags@gmail.com>
2022-08-14 16:49:22 +0200

    Re-reference

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from '../routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { PokedexComponent } from './pokedex/pokedex.component';
 import { PokeCardComponent } from './poke-card/poke-card.component';
