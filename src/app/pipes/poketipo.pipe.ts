7083bfbc3e515b97223acad9846ed0f1e3a3f04c
GitteadorZeppelin <jorgeallozags@gmail.com>
2022-08-17 19:08:19 +0200

    Comentarios en la Pipe

--- src/app/pipes/poketipo.pipe.ts
+++ src/app/pipes/poketipo.pipe.ts
@@ -6,11 +6,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PokeTipoPipe implements PipeTransform {
   //Pipe para optimizar
   transform(pkmn: any, ...args: unknown[]): any {
-    
+    //Si no tiene tipo secundario devuelve una clase u otra
     if(pkmn.types[1] == undefined){
-      return pkmn.types[0].type.name+"-primario " + pkmn.types[0].type.name+"-secundario"
+      return pkmn.types[0].type.name + "-primario " + pkmn.types[0].type.name + "-secundario"
     } else {
-      return pkmn.types[0].type.name+"-primario " + pkmn.types[1].type.name+"-secundario"
+      return pkmn.types[0].type.name + "-primario " + pkmn.types[1].type.name + "-secundario"
     }
   }
 
